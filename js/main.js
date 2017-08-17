ac2f34d22ff82c014558c56809299e9ed7ea677f
ansarimofid <ansarimofid@gmail.com>
2017-08-17 16:22:21 +0530

    Initail commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ansarimofid on 17/08/17.
+ */
+function initMap() {
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 23.156006, lng: 72.666237},
+    zoom: 13
+  });
+}
